cecbfdb28d24f08fc36c06206f8980dbdc196e7f
Prorok89 <>
2026-04-14 21:22:07 +0300

    праметры командной строки

--- image_processor/src/main.rs
+++ image_processor/src/main.rs
@@ -1,4 +1,43 @@
+use clap::{Parser, ValueEnum};
+#[derive(Parser, Debug)]
+struct Cli {
+	/// Путь к исходному PNG-изображению
+	#[arg(short, long)]
+	input : Option<String>,
+	/// Путь, по которому будет сохранено обработанное изображение
+	#[arg(short, long)]
+	output : Option<String>,
+	/// Имя плагина
+	#[arg(short, long, value_enum)]
+	plugin : PluginType,
+	/// Путь к текстовому файлу с параметрами обработки
+	#[arg(long)]
+	params : Option<String>,
+	/// Путь к директории, где находится плагин
+	#[arg(long, default_value= "target/debug")]
+	plugin_path :  Option<String>
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum PluginType {
+	Mirror,
+	Blur,
+}
+
 fn main() {
+	let args = Cli::parse();
+
+	println!("{:?}", args);
+
+	match args.plugin {
+		PluginType::Blur => {
+			println!("выбран Blur");
+		},
+		PluginType::Mirror => {
+			println!("Выбра Miror")
+		}
+	}
+
     println!("Hello, world!");
 }
 /*

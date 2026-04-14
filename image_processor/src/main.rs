e86485bbb02cbc249d854026ae67cf64759b839d
Prorok89 <>
2026-04-14 22:47:51 +0300

    Обработка парметров и чтение png файла

--- image_processor/src/main.rs
+++ image_processor/src/main.rs
@@ -1,12 +1,16 @@
+use std::{env, fs::File, io::Read, path::{Path, PathBuf}};
+use anyhow::{Result, anyhow};
+
 use clap::{Parser, ValueEnum};
+use image::GenericImageView;
 #[derive(Parser, Debug)]
 struct Cli {
 	/// Путь к исходному PNG-изображению
 	#[arg(short, long)]
-	input : Option<String>,
+	input : PathBuf,
 	/// Путь, по которому будет сохранено обработанное изображение
 	#[arg(short, long)]
-	output : Option<String>,
+	output : PathBuf,
 	/// Имя плагина
 	#[arg(short, long, value_enum)]
 	plugin : PluginType,
@@ -23,22 +27,56 @@ enum PluginType {
 	Mirror,
 	Blur,
 }
-
-fn main() {
+/*
+void process_image(
+    uint32_t width,
+    uint32_t height,
+    uint8_t* rgba_data,
+    const char* params
+); 
+*/
+fn main() -> Result<()>{
 	let args = Cli::parse();
 
 	println!("{:?}", args);
 
-	match args.plugin {
-		PluginType::Blur => {
-			println!("выбран Blur");
-		},
-		PluginType::Mirror => {
-			println!("Выбра Miror")
+	let path_input = Path::new(&args.input);
+	let path_output = Path::new(&args.output);
+	
+	if is_png(path_input)? && path_output.is_dir() {
+
+		let png = image::open(path_input)?;
+		let png_rgba = png.to_rgba8();
+		let (width, height) = png_rgba.dimensions();
+		let buf = png_rgba.into_raw();
+
+		match args.plugin {
+			PluginType::Blur => {
+				println!("выбран Blur");
+			},
+			PluginType::Mirror => {
+				println!("Выбра Miror")
+			}
 		}
 	}
+	else
+	{
+		return Err(anyhow!("Файл {:?} должен иметь расширение .png", path_input));
+	}
+    
+    Ok(())
+}
 
-    println!("Hello, world!");
+fn is_png(path: &Path) -> Result<bool>
+{
+	let mut file = File::open(path)?;
+
+	let mut header = [0u8;8];
+	_ = file.read_exact(&mut header);
+	
+	let png_header = [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A];
+
+	Ok(header == png_header)
 }
 /*
 
